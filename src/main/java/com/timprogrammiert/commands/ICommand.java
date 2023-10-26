f8c67426913ef1ff91099d3658643bba56ff3bb1
UnexperiencedDeveloper <47007198+UnexperiencedDeveloper@users.noreply.github.com>
2023-10-26 13:14:29 +0200

    Now throws CommandExecutionException

--- src/main/java/com/timprogrammiert/commands/ICommand.java
+++ src/main/java/com/timprogrammiert/commands/ICommand.java
@@ -1,10 +1,11 @@
 package com.timprogrammiert.commands;
 
+import com.timprogrammiert.commands.exceptions.CommandExecutionException;
 import com.timprogrammiert.host.Host;
 
 /**
  * @author tmatz
  */
 public interface ICommand {
-    void execute(String[] args, Host host);
+    void execute(String[] args, Host host) throws CommandExecutionException;
 }
