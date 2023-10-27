8ad3e32c7c5e47baf318fbf854a438bdebb4f3aa
UnexperiencedDeveloper <47007198+UnexperiencedDeveloper@users.noreply.github.com>
2023-10-27 17:06:33 +0200

    User needs permission to write to file and Directory to change Permission Settings

--- src/main/java/com/timprogrammiert/commands/chmod/ChmodCommand.java
+++ src/main/java/com/timprogrammiert/commands/chmod/ChmodCommand.java
@@ -6,6 +6,7 @@ import com.timprogrammiert.filesystem.FileObject;
 import com.timprogrammiert.filesystem.exceptions.FileObjectNotFoundException;
 import com.timprogrammiert.filesystem.path.Path;
 import com.timprogrammiert.filesystem.permission.FilePermission;
+import com.timprogrammiert.filesystem.permission.PermissionChecker;
 import com.timprogrammiert.filesystem.permission.PermissionUtil;
 import com.timprogrammiert.host.Host;
 
@@ -25,11 +26,17 @@ public class ChmodCommand implements ICommand {
         List<String> argList = new ArrayList<>(Arrays.asList(args));
         if(argList.isEmpty()) return; // Do nothing
         try {
+            // check Write Pem for File + Check Write Pem for Parent
             int permissionNumber = Integer.parseInt(argList.get(0));
             path = new Path(argList.get(1));
-            FileObject targetFile = path.resolvePath(host, FileObject.class);
-            FilePermission filePermission = targetFile.getFileMetaData().getFilePermission();
-            targetFile.getFileMetaData().setFilePermission(PermissionUtil.changePermission(filePermission, permissionNumber));
+            PermissionChecker permissionChecker = new PermissionChecker(path.resolvePath(host, FileObject.class),host);
+            PermissionChecker permissionCheckerParent = new PermissionChecker(path.resolvePath(host, FileObject.class).getParent(), host);
+            if(permissionChecker.isCanWrite() && permissionCheckerParent.isCanWrite()){
+                FileObject targetFile = path.resolvePath(host, FileObject.class);
+                FilePermission filePermission = targetFile.getFileMetaData().getFilePermission();
+                targetFile.getFileMetaData().setFilePermission(PermissionUtil.changePermission(filePermission, permissionNumber));
+            }
+
         } catch (NumberFormatException e) {
             System.out.println(e.getMessage());
         } catch (FileObjectNotFoundException e) {
