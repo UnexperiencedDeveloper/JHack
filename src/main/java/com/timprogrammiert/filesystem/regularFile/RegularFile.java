3a6ed618ea67035c2701bc7f7f176b4d4dfc77d7
UnexperiencedDeveloper <47007198+UnexperiencedDeveloper@users.noreply.github.com>
2023-10-27 17:07:49 +0200

    Fixed a bug where fileContent starts with "null"

--- src/main/java/com/timprogrammiert/filesystem/regularFile/RegularFile.java
+++ src/main/java/com/timprogrammiert/filesystem/regularFile/RegularFile.java
@@ -1,6 +1,5 @@
 package com.timprogrammiert.filesystem.regularFile;
 
-import com.timprogrammiert.filesystem.FileMetaData;
 import com.timprogrammiert.filesystem.FileObject;
 import com.timprogrammiert.filesystem.directory.Directory;
 import com.timprogrammiert.user.User;
@@ -10,7 +9,7 @@ import com.timprogrammiert.util.FileType;
  * @author tmatz
  */
 public class RegularFile extends FileObject {
-    private String fileContent;
+    private String fileContent = "";
 
     public static RegularFile createRegularFile(String fileName, FileType fileType, User user, Directory parent){
         return new RegularFile(fileName,fileType, user, parent);
