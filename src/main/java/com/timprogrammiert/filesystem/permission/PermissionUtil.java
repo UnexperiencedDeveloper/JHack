a96a1585fee180a474a199be5336ab0e922ac4fe
Tim <tmatzenauer@hotmail.de>
2023-10-29 12:54:49 +0100

    Added comments

--- src/main/java/com/timprogrammiert/filesystem/permission/PermissionUtil.java
+++ src/main/java/com/timprogrammiert/filesystem/permission/PermissionUtil.java
@@ -1,13 +1,21 @@
 package com.timprogrammiert.filesystem.permission;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
+ *  The `PermissionUtil` class provides utility methods to handle and modify file permissions.
+ *  It contains static methods for changing permission strings based on numeric representations,
+ *  allowing for easy conversion between numerical permissions and their symbolic representations.
  * @author tmatz
+ * @version 1.0
  */
 public class PermissionUtil {
-    private final static Logger logger = Logger.getLogger(PermissionUtil.class.getName());
+    /**
+     * Changes the file permission based on the given numeric representation.
+     *
+     * @param originalPermission The original FilePermission object to be modified.
+     * @param permissionNumber   The numeric representation of the permission (e.g., "755").
+     * @return The updated FilePermission object after applying the numeric permissions.
+     */
     public static FilePermission changePermission(FilePermission originalPermission, String permissionNumber){
         String ownerPermission = String.valueOf(permissionNumber).substring(0,1);
         String groupPermission = String.valueOf(permissionNumber).substring(1,2);
@@ -19,6 +27,12 @@ public class PermissionUtil {
         return originalPermission;
     }
 
+    /**
+     * Determines the symbolic representation of a numeric permission digit.
+     *
+     * @param permissionNumber The numeric permission digit (0 to 7).
+     * @return The symbolic representation of the permission digit (e.g., "---", "rwx").
+     */
     private static String determinePermission(String permissionNumber){
         return switch (permissionNumber) {
             case "0" -> "---";
