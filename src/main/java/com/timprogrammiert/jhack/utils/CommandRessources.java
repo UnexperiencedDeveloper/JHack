4982aecaf5724a4ae5ed51560036e376fdba3de4
Tim <tmatzenauer@hotmail.de>
2024-02-20 19:45:53 +0100

    Add RmCommand

--- src/main/java/com/timprogrammiert/jhack/utils/CommandRessources.java
+++ src/main/java/com/timprogrammiert/jhack/utils/CommandRessources.java
@@ -14,4 +14,5 @@ public class CommandRessources {
     public static final String LS_USAGE = "USAGE LS";
     public static final String MV_USAGE = "USAGE MV";
     public static final String CP_USAGE = "USAGE CP";
+    public static final String RM_USAGE = "USAGE RM";
 }
